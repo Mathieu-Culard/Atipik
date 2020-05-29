9150d790c7809a9c8c289940e43a9a794cccb49d
Kevin Lanvin <info.lanvink@gmail.com>
2020-05-29 12:22:08 +0200

    Send axios request and store result

--- frontend/src/middlewares/userMiddleware.js
+++ frontend/src/middlewares/userMiddleware.js
@@ -1,14 +1,18 @@
 import { LOG_IN, loginChanged, LOG_OUT } from 'src/actions/user';
+import axios from 'axios';
 
 const userMiddleware = (store) => (next) => (action) => {
   switch (action.type) {
     case LOG_IN: {
       const { email, password } = store.getState().user;
-      // TODO Login request
-      console.log(`Sending login request to ${process.env.REACT_APP_API_URL} with email: ${email} password: ${password}`);
-      // on success :
-      // localStorage.setItem('jwt', response.jwt);
-      // store.dispatch(loginChanged());
+      console.log(`${process.env.REACT_APP_BACKEND_URL}/login_check`);
+      axios.post(`http://${process.env.REACT_APP_BACKEND_URL}/login_check`, {
+        username: email,
+        password,
+      }).then((response) => {
+        localStorage.setItem('jwt', response.token);
+        store.dispatch(loginChanged());
+      });
       next(action);
       break;
     }
