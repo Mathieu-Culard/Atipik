43681f4b5aa565a09a2c67f1d8cf91e61a44b659
Mathieu-Culard <culard.mathieu@gmail.com>
2020-05-29 10:07:35 +0200

    fixes

--- frontend/src/components/App/index.js
+++ frontend/src/components/App/index.js
@@ -4,22 +4,24 @@ import PropTypes from 'prop-types';
 import './app.scss';
 import Header from 'src/containers/Header';
 import Footer from 'src/components/Footer';
+import LoginPanel from 'src/containers/LoginPanel';
 
 const App = ({ checkLogged }) => {
-  useEffect(() => {
-    checkLogged();
-  }, []);
+  // useEffect(() => {
+  //   checkLogged();
+  // }, []);
   return (
     <div className="app">
       <Header />
+      <LoginPanel />
       <Footer />
     </div>
   );
 };
 
-App.propTypes = {
-  checkLogged: PropTypes.func.isRequired,
-};
+// App.propTypes = {
+//   checkLogged: PropTypes.func.isRequired,
+// };
 
 // == Export
 export default App;
