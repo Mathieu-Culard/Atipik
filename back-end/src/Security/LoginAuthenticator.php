396157372c26b49a75747db64b956eb69b8e4944
elisa-crusoe <59163242+elisa-crusoe@users.noreply.github.com>
2020-06-03 09:29:47 +0200

    modif' ok

--- back-end/src/Security/LoginAuthenticator.php
+++ back-end/src/Security/LoginAuthenticator.php
@@ -97,7 +97,7 @@ class LoginAuthenticator extends AbstractFormLoginAuthenticator implements Passw
         }
 
         // For example : 
-        return new RedirectResponse($this->urlGenerator->generate('admin_type_browse'));
+        return new RedirectResponse($this->urlGenerator->generate('admin_validate_accomodations_browse'));
         throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
