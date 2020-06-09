4d15fd52c323d7dabc17bd15b8a55c5213079182
Kevin Lanvin <info.lanvink@gmail.com>
2020-06-09 11:28:26 +0200

    Add action management for reservations

--- frontend/src/middlewares/reservationMiddleware.js
+++ frontend/src/middlewares/reservationMiddleware.js
@@ -1,11 +1,15 @@
-// import axios from 'axios';
+import axios from 'axios';
+
+import { FETCH_RESERVATIONS, saveReservations } from 'src/actions/reservations';
 
 const reservationMiddleware = (store) => (next) => (action) => {
   switch (action.type) {
-    // case FETCH_RESERVATIONS: {
-    //   axios.get(`${process.env.REACT_APP_API_URL}/reservations`);
-    // }
-    //   break;
+    case FETCH_RESERVATIONS:
+      axios.get(`${process.env.REACT_APP_API_URL}/reservations`)
+        .then((response) => {
+          store.dispatch(saveReservations(response.data));
+        });
+      break;
 
     default: next(action);
   }
